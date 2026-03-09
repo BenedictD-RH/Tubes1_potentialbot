bf6f1ca205fc2438e9f92e520bdb4c301fc0157c
BenedictD-RH <benedictdarrel572006@gmail.com>
2026-03-09 16:36:59 +0700

    fix: fixed removeLocation when savedLocations is empty

---
+++ src/towerRusher/LocationMemory.java
@@ -0,0 +1,67 @@
+package towerRusher;
+
+import battlecode.common.*;
+
+public class LocationMemory {
+    public int amountSaved;
+    public MapLocation[] savedLocations = new MapLocation[20];
+
+    public LocationMemory() {
+        amountSaved = 0;
+    }
+
+    public boolean isLocationInMemory(MapLocation m) {
+        boolean found = false;
+        for (int i = 0; i < amountSaved; i++) {
+            found = savedLocations[i].equals(m);
+            if (found) break;
+        }
+        return found;
+    }
+
+    public boolean isLocationInMemory(int x, int y) {
+        return isLocationInMemory(new MapLocation(x, y));
+    }
+
+    public void saveLocation(MapLocation m) {
+        if (!isLocationInMemory(m)) {
+            savedLocations[amountSaved] = m;
+            amountSaved++;
+        }
+    }
+
+    public void saveLocation(int x, int y) {
+        saveLocation(new MapLocation(x, y));
+    }
+
+    public void removeLocation(MapLocation m) {
+        if (isLocationInMemory(m)) {
+            int i = 0;
+            while (!savedLocations[i].equals(m) && i < amountSaved) {
+                i++;
+            }
+            if (i >= amountSaved) {
+                return;
+            }
+            amountSaved--;
+            while (i < amountSaved) {
+                savedLocations[i] = savedLocations[i + 1];
+                i++;
+            }
+        }
+    }
+
+    public void removeLocation(int x, int y) {
+        removeLocation(new MapLocation(x, y));
+    }
+
+    public MapLocation nearestLocationTo(MapLocation m) {
+        MapLocation currNearest = savedLocations[0];
+        for(int i = 0; i < amountSaved; i++) { 
+            if(currNearest.distanceSquaredTo(m) > savedLocations[i].distanceSquaredTo(m)) {
+                currNearest = savedLocations[i];
+            }
+        }
+        return currNearest;
+    }
+}
