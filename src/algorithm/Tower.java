3912ba53e5502342164abd3e2c70f733baced8f1
Alpaomega1136 <199881447+Alpaomega1136@users.noreply.github.com>
2026-03-12 02:41:24 +0700

    feat : add mopper algortihm and defense & upgrade system

--- src/algorithm/Tower.java
+++ src/algorithm/Tower.java
@@ -3,17 +3,43 @@ package algorithm;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
+import battlecode.common.RobotInfo;
 import battlecode.common.UnitType;
 
 public class Tower extends BaseRobot {
     
     public static void run() throws GameActionException {
-        if (rc.isActionReady()) {
-            spawnBot();
+        // Defense system for defense towers
+        if (rc.getType() == UnitType.LEVEL_ONE_DEFENSE_TOWER ||
+            rc.getType() == UnitType.LEVEL_TWO_DEFENSE_TOWER ||
+            rc.getType() == UnitType.LEVEL_THREE_DEFENSE_TOWER) {
+            
+            RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+            if (enemies.length > 0 && rc.isActionReady()) {
+                MapLocation targetEnemy = enemies[0].getLocation();
+                if (rc.canAttack(targetEnemy)) {
+                    rc.attack(targetEnemy);
+                    System.out.println("Defense tower attacked enemy at " + targetEnemy);
+                    return;
+                }
+            }
+        }
+
+        // Upgrade system 
+        if (rc.getMoney() > 2500 && rc.isActionReady()) {
+            if (rc.canUpgradeTower(rc.getLocation())) {
+                rc.upgradeTower(rc.getLocation());
+                return;
+            }
+        }
+
+        // Robot production
+        if (rc.isActionReady() && rc.getMoney() >= 500) {
+            buildArmy();
         }
     }
 
-    private static void spawnBot() throws GameActionException {
+    private static void buildArmy() throws GameActionException {
         double rand = Math.random();
         UnitType typeToBuild;
 
--- src/algorithm/Units/Mopper.java
+++ src/algorithm/Units/Mopper.java
@@ -3,34 +3,61 @@ package algorithm.Units;
 import algorithm.Unit;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.MapInfo;
 import battlecode.common.MapLocation;
+import battlecode.common.PaintType;
 import battlecode.common.RobotInfo;
 
 
 public class Mopper extends Unit {
     public static void run() throws GameActionException{
-        RobotInfo[] enemies = rc.senseNearbyRobots(-1,rc.getTeam().opponent());
+        initUnit();
+        if(refillPaint()){
+            return;
+        }
 
+        RobotInfo[] enemies = rc.senseNearbyRobots(-1,rc.getTeam().opponent());
+        MapInfo[] nearbyTiles = rc.senseNearbyMapInfos(-1);
+
+        // PRIORITY 1: attack nearby enemies
         if(enemies.length > 0){
             MapLocation targetEnemy = enemies[0].getLocation();
-
-            if(rc.canAttack(targetEnemy)){
+            if (rc.isActionReady() && rc.canAttack(targetEnemy)) {
                 rc.attack(targetEnemy);
-            }
-            else{
+            } else if(rc.isMovementReady()){
                 move(targetEnemy);
             }
+            return;
         }
-        else{
+
+        // PRIORITY 2: clean enemy paint
+        MapLocation bestTileToMop = null;
+        int minDistance = Integer.MAX_VALUE;
+        for (MapInfo tile : nearbyTiles) {
+            PaintType paint = tile.getPaint();
+            if(!paint.isAlly() && paint != PaintType.EMPTY){
+                int dist = rc.getLocation().distanceSquaredTo(tile.getMapLocation());
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    bestTileToMop = tile.getMapLocation();
+                }
+            
+            }
+        }
+
+        if(bestTileToMop != null){
+            if (rc.isActionReady() && rc.canAttack(bestTileToMop)) {
+                rc.attack(bestTileToMop);
+            } else if(rc.isMovementReady()){
+                move(bestTileToMop);
+            }
+        }
+        else if(rc.isMovementReady()){
             wander();
         }
     }
 
     private static void wander() throws GameActionException {
-        if (!rc.isMovementReady()) {
-            return;
-        }
-
         int randomIndex = (int) (Math.random() * directions.length);
         Direction randomDir = directions[randomIndex];
 
